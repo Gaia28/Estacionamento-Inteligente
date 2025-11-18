01e221f143f5ec4a10eb518f1d4585962dde8c63
Gaia28 <matheusgaia33@gmail.com>
2025-11-18 15:00:06 -0300

    feat: new route to manage users

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ session_start();
 
 require_once 'controllers/AuthController.php';
 require_once 'controllers/MonitorController.php';
+require_once 'controllers/UserController.php';
 
 // Define a rota padrão
 $route = $_GET['route'] ?? 'monitor';
 
 $authController = new AuthController();
 $monitorController = new MonitorController();
+$userController = new UserController();
 
 switch ($route) {
     case 'login':
@@ -16,6 +18,9 @@ switch ($route) {
         break;
     case 'logout':
         $authController->handleLogout();
+        break;
+    case 'manage_users': 
+        $userController->manageUsers();
         break;
     case 'monitor':
     default:
